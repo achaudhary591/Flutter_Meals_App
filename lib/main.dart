6107ff1c3ea290f0f63cf86e634eb1962b7da63b
achaudhary591 <achaudhary591@gmail.com>
2023-02-27 15:11:03 +0530

    started again

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import './category_meals_screen.dart';
 import './categories_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
